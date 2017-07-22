1ba8491f8acc52c3c64138e06d0874ef11e9b52f
Ethan Li <ethan.li@titansoft.com.sg>
2017-07-22 20:46:43 +0800

    initialize project

---
+++ PrimeString/KataTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PrimeString
+{
+    [TestFixture]
+    public class KataTest
+    {
+        [TestCase("x", ExpectedResult = true)]
+        [TestCase("abc", ExpectedResult = true)]
+        [TestCase("abac", ExpectedResult = true)]
+        [TestCase("abab", ExpectedResult = false)]
+        [TestCase("aaaa", ExpectedResult = false)]
+        [TestCase("abba", ExpectedResult = true)]
+        [TestCase("utdutdtdutd", ExpectedResult = true)]
+        [TestCase("fdsyffdsyffdsyffdsyffdsyf", ExpectedResult = false)]
+        public bool BasicTests(string input)
+        {
+            var kata = new Kata();
+            return kata.PrimeString(input);
+        }
+    }
+}
